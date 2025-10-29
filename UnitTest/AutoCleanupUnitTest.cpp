ebc5896a31f513551af2cda7d24409be7844f40e
mingkuang <mingkuang@live.com>
2025-10-29 16:26:59 +0800

    Fea, 添加AutoCleanup

---
+++ UnitTest/AutoCleanupUnitTest.cpp
@@ -0,0 +1,28 @@
+﻿#include "CppUnitTest.h"
+
+#include <YY/Base/Utils/AutoCleanup.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+    TEST_CLASS(AutoCleanup)
+    {
+    public:
+        TEST_METHOD(普通)
+        {
+            int _iValue = 0;
+
+            {
+                auto _oCleanup = YY::MakeAutoCleanup(
+                    [&]()
+                    {
+                        _iValue++;
+                    });
+
+                Assert::AreEqual(0, _iValue);
+            }
+            Assert::AreEqual(1, _iValue);
+        }
+    };
+}
